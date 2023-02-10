57e58854d7a0e1be82bb187e5372db39565d6777
Rajnikanth336 <46199913+Rajnikanthyadav336@users.noreply.github.com>
2023-02-10 14:12:48 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "backend-s3" {
+  bucket = "drithirajni-s3-backend" 
+  force_destroy = "true"
+  
+}
+resource "aws_s3_bucket_versioning" "backend-s3-version" {
+  bucket = aws_s3_bucket.backend-s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "backend-dynamoDB" {
+    name           = "backend1"
+    billing_mode   = "PROVISIONED"
+    read_capacity  = 10
+    write_capacity = 10
+    hash_key       = "LockID"
+
+    attribute {
+     name = "LockID"
+     type = "S"
+   }
+   }
